4f77dda8d323ae428f69cf16072279ec5fc11f83
Jonmorais <jonathan_ms15@hotmail.com>
2021-10-01 22:33:16 -0300

    Teste de mudança no import do useParams

--- src/pages/DrinkDetails.jsx
+++ src/pages/DrinkDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import { useParams } from 'react-router-dom';
 
 import shareIcon from '../images/shareIcon.svg';
 import isNotFavoriteIcon from '../images/whiteHeartIcon.svg';
