df7c391d9e62473c8f35b579a6a85b7845240a39
Carol_Inglethe <carolinainglethe@gmail.com>
2021-10-20 21:53:26 -0300

    css tela login , e inicio de css tela principal receitas

--- src/pages/Food.jsx
+++ src/pages/Food.jsx
@@ -1,7 +1,5 @@
 import React, { useContext } from 'react';
-
 import RecipesAppContext from '../context/RecipesAppContext';
-
 import HeaderWithSearchFood from '../components/HeaderWithSearchFood';
 import Footer from '../components/Footer';
 import FoodCards from '../components/FoodCards';
@@ -26,7 +24,7 @@ function Food() {
   }
   if (selectedFoodIngredient === '' || filteredFoods.length > 1) {
     return (
-      <div>
+      <div className="page-main-food">
         <HeaderWithSearchFood titlePage="Comidas" />
         <div>
           <FoodCategoryButtons />
@@ -37,7 +35,7 @@ function Food() {
     );
   }
   return (
-    <div>
+    <div className="page-main-food">
       <HeaderWithSearchFood titlePage="Comidas" />
       <div>
         <p>Tela principal de Receitas:</p>
